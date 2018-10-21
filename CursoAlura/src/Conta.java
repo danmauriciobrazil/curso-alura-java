ae96153562523fbf39cae9c860289c2c700c13c5
spiderman <danilo.mauricio.silva@gmail.com>
2018-10-21 18:12:20 -0300

    classe client

--- CursoAlura/src/Conta.java
+++ CursoAlura/src/Conta.java
@@ -1,12 +1,34 @@
 public class Conta {
 
-	int agencia;
-	double saldo;
-	int numero;
-	String titular;
+	private int agencia;
+	private double saldo;
+	private int numero;
+	Cliente titular ;
 	
-	void deposita(double valor){
-		
+	public double deposita(double valor){
+		return this.saldo += valor;
+	}
+	
+	public double pegaSaldo(){
+		return this.saldo;
+	}
+	public boolean saca (double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}else {
+			return false;
+		}	
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}else {
+			return false;
+			}
 	}
 
 	public int getAgencia() {
@@ -33,12 +55,5 @@ public class Conta {
 		this.numero = numero;
 	}
 
-	public String getTitular() {
-		return titular;
-	}
-
-	public void setTitular(String titular) {
-		this.titular = titular;
-	}
 
 }
--- CursoAlura/src/CriarConta.java
+++ CursoAlura/src/CriarConta.java
@@ -3,22 +3,9 @@ public class CriarConta {
 
 	public static void main(String[] args) {
 		
-		Conta primeiraConta = new Conta();
-		Conta segundaConta = new Conta();
-	
-		primeiraConta.saldo +=200;
-		primeiraConta.agencia =1;
-		primeiraConta.numero =1234;
-		primeiraConta.titular = "Danilo";
-		primeiraConta.saldo +=200;
-		System.out.println("Primeira Conta " + primeiraConta.saldo);
-		
-		segundaConta.saldo +=50;
-		segundaConta.agencia =1;
-		segundaConta.numero =1234;
-		segundaConta.titular = "Danilo";
-		segundaConta.saldo +=200;
-		System.out.println("Segunda Conta " + segundaConta.saldo);
-		
+	Conta contaDoDanilo = new Conta();
+	contaDoDanilo.deposita(100);
+	System.out.println("Saldo disponível : " + contaDoDanilo.pegaSaldo());
 	}
+	
 }
