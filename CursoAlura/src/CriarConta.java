93ef6ebae96c98ba1025017a6068726822f8deda
danilomauriciodasilva <danilo.mauricio.silva@gmail.com>
2023-10-03 19:27:09 -0300

    curso alura teste

--- CursoAlura/src/CriarConta.java
+++ CursoAlura/src/CriarConta.java
@@ -2,7 +2,7 @@
 public class CriarConta {
 
 	public static void main(String[] args) {
-		
+		//curso alura
 	Conta contaDoDanilo = new Conta(123,5656);
 	contaDoDanilo.deposita(100);
 	System.out.println("Saldo disponível : " + contaDoDanilo.pegaSaldo());
@@ -14,7 +14,11 @@ public class CriarConta {
 	Conta contaDoPaulo = new Conta(123,5656);
 	contaDoPaulo.deposita(400);
 	System.out.println("Saldo disponível : " + contaDoPaulo.pegaSaldo());
-	
+
+	Conta contaDoPaulo = new Conta(12332,5656);
+		contaDoPaulo.deposita(400);
+		System.out.println("Saldo disponível : " + contaDoPaulo.pegaSaldo());
+
 	Funcionarios funcDanilo = new Funcionarios();
 	funcDanilo.setSalário(145.00);
 	funcDanilo.setNome("Danilo");
